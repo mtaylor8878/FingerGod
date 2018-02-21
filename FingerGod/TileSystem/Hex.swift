d4648d6ec4cfcf35b6493bed683fd269ad2898ab
Niko Lauron <niko@Nikos-iMac.local>
2018-02-20 22:01:09 -0800

    Finished Hex Class and made Map Class

---
+++ FingerGod/TileSystem/Hex.swift
@@ -0,0 +1,23 @@
+
+//
+//  Hex.swift
+//  FingerGod
+//
+//  Created by Niko Lauron on 2018-02-18.
+//  Copyright © 2018 Ramen Interactive. All rights reserved.
+//
+
+import Foundation
+
+/*
+ Data structure for cube coordinates and the algorithms that work with them
+ */
+public class Hex {
+    
+    /*
+     
+    */
+    struct offset {
+        	
+    }
+}
