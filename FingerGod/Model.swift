49ed845c088d7b9eda9b64c0c05fdf12ea19d16d
Darkblizer <darkblizer@gmail.com>
2018-02-21 10:16:15 -0800

    Did some basic class setup

---
+++ FingerGod/Model.swift
@@ -0,0 +1,10 @@
+//
+//  Renderer.swift
+//  FingerGod
+//
+//  Created by Aaron Freytag on 2018-02-21.
+//  Copyright © 2018 Ramen Interactive. All rights reserved.
+//
+
+public class Model : Component {
+}
---
+++ FingerGod/Renderer.swift
@@ -0,0 +1,21 @@
+//
+//  Renderer.swift
+//  FingerGod
+//
+//  Created by Aaron Freytag on 2018-02-21.
+//  Copyright © 2018 Ramen Interactive. All rights reserved.
+//
+
+public class Renderer {
+	private static var models: [Model] = []
+	
+	public static func setup(GLKView* view) {
+	}
+	
+	public static func draw(CGRect drawRect) {
+	}
+
+	public static func addModel(Model* model) {
+	}
+		
+}
