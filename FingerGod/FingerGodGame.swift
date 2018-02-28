a187465f9773d9971085961443ada1ca4b8053e4
Aaron Freytag <aaroncfreytag@gmail.com>
2018-02-27 23:54:50 -0800

    Got the game to create and update, along with creating some abstraction classes

---
+++ FingerGod/FingerGodGame.swift
@@ -0,0 +1,19 @@
+//
+//  FingerGodGame.swift
+//  FingerGod
+//
+//  Created by Aaron F on 2018-02-27.
+//  Copyright © 2018 Ramen Interactive. All rights reserved.
+//
+
+import Foundation
+
+public class FingerGodGame : Game {
+    public override func onGameStart() {
+        super.onGameStart()
+        let sampleObj = GameObject(id: 0)
+        
+        sampleObj.addComponent(type: RotatingCubeComponent.self)
+        self.addGameObject(gameObject: sampleObj)
+    }
+}
--- FingerGod/ViewController.swift
+++ FingerGod/ViewController.swift
@@ -6,41 +6,34 @@
 //  Copyright © 2018 Ramen Interactive. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import GLKit
 
 class ViewController: GLKViewController {
-
+    
     private var i : ModelInstance!
+    private var i2 : ModelInstance!
+    
+    private var game : Game!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         Renderer.setup(view: self.view as! GLKView)
-        
-        do {
-            let m = try ModelReader.read(objPath: "HexTile")
-            i = ModelInstance(model: m)
-            
-            i.transform = GLKMatrix4Translate(i.transform, 0, 0, -5.0)
-            i.color = [0.2, 0.95, 0.55, 1.0]
-            
-            Renderer.addInstance(inst: i)
-        }
-        catch {
-            
-        }
+        game = FingerGodGame()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func update() {
     }
-    
     override func glkView(_ view: GLKView, drawIn rect: CGRect) {
-        i.transform = GLKMatrix4Rotate(i.transform, Float.pi / 100, 1, 0, 0)
+        super.glkView(view, drawIn: rect)
+        game.update()
         Renderer.draw(drawRect: rect)
     }
 
