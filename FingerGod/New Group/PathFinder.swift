e0a6199344fe84884564d283a96d5f1bf2ff9091
Aaron Freytag <aaroncfreytag@gmail.com>
2018-04-15 01:59:39 -0700

    Wrote pathfinding code, yet to test it

--- FingerGod/New Group/PathFinder.swift
+++ FingerGod/New Group/PathFinder.swift
@@ -9,11 +9,77 @@
 import Foundation
 
 public class PathFinder {
-    public static func getPath(start:Tile, end:Tile) -> [Tile] {
-        var tiles = [Tile]()
+    public static func getPath(start:(x: Int, y: Int), end:(x: Int, y: Int), map: TileMap) -> [(x: Int, y: Int)] {
+        // Begin A* Pathfinding Code
+        // Adapted from http://www.redblobgames.com/pathfinding/a-star/introduction.html
+        let frontier = PriorityQueue<Tile>()
+        var from = [Point2D : Point2D]() // Which point did from[point] come from? Note: Key is Point2D since it's hashable
+        var cost = [Point2D : Float]() // What did it cost to get to tile? Note: Key is Point2D since it's hashable
+        let endTile = map.getTile(end.x, end.y)
         
-        tiles.append(start)
+        if (endTile == nil) {
+            return []
+        }
         
-        return tiles
+        let startPoint = Point2D(start.x, start.y)
+        cost[startPoint] = 0
+        frontier.add(item: map.getTile(startPoint)!, priority: cost[startPoint]!)
+        
+        while frontier.count > 0 {
+            let curr = frontier.shift()
+            
+            if (curr.getAxial().x == end.x && curr.getAxial().y == end.y) {
+                break
+            }
+            
+            let neighbours = getNeighbours(t: curr, map: map)
+            
+            for next in neighbours {
+                let newCost = cost[curr.getAxial()]! + getCost(next)
+                let nextCost = cost[next.getAxial()]
+                
+                if nextCost == nil || newCost < nextCost! {
+                    cost[next.getAxial()] = newCost
+                    frontier.add(item: next, priority: newCost + distance(endTile!, next))
+                    from[next.getAxial()] = curr.getAxial()
+                }
+            }
+        }
+        
+        var path = [end]
+        
+        // Backtracking to figure out path
+        
+        var step = Point2D(end.x, end.y)
+        // As long as we can keep going back
+        while from[step] != nil {
+            // Move back and add the previous spot to the list
+            step = from[step]!
+            path.append((x: step.x, y: step.y))
+        }
+        
+        return path
+    }
+    
+    public static func getNeighbours(t: Tile, map: TileMap) -> [Tile] {
+        let pos = t.getAxial()
+        var neighbours : [Tile] = []
+        for offset in [Point2D(-1, 0), Point2D(-1, 1), Point2D(0, 1), Point2D(1, 0), Point2D(1, -1), Point2D(0, -1)] {
+            if let t = map.getTile(Point2D(pos.x + offset.x, pos.y + offset.y)) {
+                neighbours.append(t)
+            }
+        }
+        return neighbours
+    }
+    
+    public static func getCost(_ tile: Tile) -> Float {
+        return 1
+    }
+    
+    public static func distance(_ tileA: Tile, _ tileB: Tile) -> Float {
+        let ca = tileA.worldCoordinate;
+        let cb = tileB.worldCoordinate;
+        let c = (x: ca.x - cb.x, y: ca.y - cb.y)
+        return (c.x * c.x + c.y * c.y).squareRoot()
     }
 }
