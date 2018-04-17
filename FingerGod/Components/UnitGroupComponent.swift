55fef60e72d6d5c140dec7db131cceeead6a056d
Aaron Freytag <aaroncfreytag@gmail.com>
2018-04-16 23:31:09 -0700

    Prepared to render multiple models for unit groups

--- FingerGod/Components/UnitGroupComponent.swift
+++ FingerGod/Components/UnitGroupComponent.swift
@@ -12,7 +12,10 @@ import GLKit
 public class UnitGroupComponent : Component {
     // Axial coordinate position
     public var position = [0, 0]
+    private var model : Model?
+    private var unitModels = [ModelInstance]()
     private var modelInst : ModelInstance?
+    private var squareSize = 0
     var unitGroup = UnitGroup.initUnitGroupWith(peopleNum:10, followerNum: 0, demiGodNum: 0)
     
     private var initShape = GLKMatrix4Scale(GLKMatrix4Identity, 0.5, 0.5, 0.5)
@@ -21,12 +24,8 @@ public class UnitGroupComponent : Component {
     public override func create() {
         print("Creating Unit Group")
         do {
-            let model = try ModelReader.read(objPath: "CubeModel")
-            modelInst = ModelInstance(model: model)
-            modelInst?.transform = GLKMatrix4Translate(initShape, 0, 0.75, 0);
-            modelInst?.transform = GLKMatrix4Multiply((modelInst?.transform)!, initShape)
-            modelInst?.color = [1.0, 1.0, 1.0, 1.0]
-            Renderer.addInstance(inst: modelInst!)
+            model = try ModelReader.read(objPath: "CubeModel")
+            updateModels()
         } catch {
             print("There was a problem: \(error)")
         }
@@ -76,5 +75,13 @@ public class UnitGroupComponent : Component {
         let z = Float(3).squareRoot() * (Float(r) + Float(q) / 2) // z value
         
         return (x,z)
+    }
+    
+    private func updateModels() {
+        modelInst = ModelInstance(model: model!)
+        modelInst?.transform = GLKMatrix4Translate(initShape, 0, 0.75, 0);
+        modelInst?.transform = GLKMatrix4Multiply((modelInst?.transform)!, initShape)
+        modelInst?.color = [1.0, 1.0, 1.0, 1.0]
+        Renderer.addInstance(inst: modelInst!)
     }
 }
